8508949c6d61f5ca8690da6349e7d0aa04ae6875
Dennie Froese <dennie.froese@santander.co.uk>
2020-07-10 16:13:01 +0100

    implementation of filter menu to filter for black products

--- App.tsx
+++ App.tsx
@@ -1,9 +1,16 @@
 import React, {useState, useEffect} from 'react';
-import {StyleSheet, ScrollView} from 'react-native';
+import {
+  StyleSheet,
+  View,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
 import Item, {Product} from 'components/Item';
 
 export default function App() {
   const [products, setProducts] = useState();
+  const [open, setOpen] = useState(false);
 
   useEffect(() => {
     fetch('https://my-json-server.typicode.com/benirvingplt/products/products')
@@ -13,18 +20,41 @@ export default function App() {
       });
   }, []);
 
-  console.warn(products);
+  const filter = () => {
+    let filteredProducts: Product[] = products.filter((product: Product) => {
+      return product.colour.toLowerCase().match('black');
+    });
+    setProducts(filteredProducts);
+    setOpen(false);
+  };
 
   return (
-    <>
-      <ScrollView style={styles.container}>
+    <View style={styles.container}>
+      <View style={styles.filterContainer}>
+        <TouchableOpacity style={styles.filter} onPress={() => setOpen(!open)}>
+          <Text style={styles.text}>Colour filter</Text>
+        </TouchableOpacity>
+        {open ? (
+          <View>
+            <TouchableOpacity onPress={filter}>
+              <Text>BLACK</Text>
+            </TouchableOpacity>
+
+            <Text>RED</Text>
+            <Text>STONE</Text>
+          </View>
+        ) : null}
+      </View>
+      <ScrollView>
         {products
           ? products.map((product: Product) => {
-              return <Item product={product} />;
+              return (
+                <Item product={product ? product : null} key={product.id} />
+              );
             })
           : null}
       </ScrollView>
-    </>
+    </View>
   );
 }
 
@@ -32,5 +62,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 100,
+  },
+  filterContainer: {
+    width: 100,
+    marginLeft: 30,
+  },
+  filter: {
+    borderColor: 'black',
+    borderWidth: 1,
+  },
+  text: {
+    textAlign: 'center',
   },
 });
