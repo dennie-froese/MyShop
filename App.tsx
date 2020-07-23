69acd70347fcaf2523a1ba44c6d46c8ca7b8cfce
Dennie Froese <dennie.froese@santander.co.uk>
2020-07-23 17:08:29 +0100

    simplified the logic in App.tsx, implemented ItemList component in it and wrapped in new TotalProvider

--- App.tsx
+++ App.tsx
@@ -1,119 +1,16 @@
 import React, {useState, useEffect} from 'react';
-import {
-  StyleSheet,
-  View,
-  ScrollView,
-  Text,
-  TouchableOpacity,
-} from 'react-native';
-import Item, {Product} from 'components/Item';
+import {StyleSheet} from 'react-native';
+import {TotalProvider} from 'hooks/useTotalContext';
+import ItemList from 'components/ItemList';
 
 export default function App() {
-  const [products, setProducts] = useState();
-  const [open, setOpen] = useState(false);
-  const [total, setTotal] = useState(0);
-
-  useEffect(() => {
-    fetch('https://my-json-server.typicode.com/benirvingplt/products/products')
-      .then((res) => res.json())
-      .then((result) => {
-        setProducts(result);
-      });
-  }, []);
-
-  const filter = () => {
-    const filteredProducts: Product[] = products.filter((product: Product) => {
-      return product.colour.toLowerCase().match('black');
-    });
-    setProducts(filteredProducts);
-    setOpen(false);
-  };
-
-  function increment(n: number) {
-    setTotal(total + n);
-  }
-
-  function decrement(n: number) {
-    setTotal(total - n);
-  }
-
-  function remove(id: number) {
-    const tempProductsReduced = products.filter(function (product: Product) {
-      return product.id != id;
-    });
-    setProducts(tempProductsReduced);
-  }
-
   return (
     <>
-      {!products ? (
-        <Text>Oops - something went wrong!</Text>
-      ) : (
-        <View style={styles.container}>
-          <View style={styles.filterContainer}>
-            <TouchableOpacity
-              style={styles.filter}
-              onPress={() => setOpen(!open)}>
-              <Text style={styles.text}>Colour filter</Text>
-            </TouchableOpacity>
-            {open ? (
-              <View>
-                <TouchableOpacity onPress={filter}>
-                  <Text>BLACK</Text>
-                </TouchableOpacity>
-              </View>
-            ) : null}
-          </View>
-          <ScrollView>
-            {products
-              ? products.map((product: Product) => {
-                  return (
-                    <Item
-                      product={product ? product : null}
-                      key={product.id}
-                      increment={increment}
-                      decrement={decrement}
-                      remove={remove}
-                    />
-                  );
-                })
-              : null}
-          </ScrollView>
-          <View style={styles.total}>
-            <View style={styles.totalView}>
-              <Text style={styles.text}>Total: {total.toFixed(2)}£</Text>
-            </View>
-          </View>
-        </View>
-      )}
+      <TotalProvider>
+        <ItemList />
+      </TotalProvider>
     </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 100,
-  },
-  filterContainer: {
-    width: 100,
-    marginLeft: 30,
-  },
-  filter: {
-    borderColor: 'black',
-    borderWidth: 1,
-  },
-  text: {
-    textAlign: 'center',
-  },
-  total: {
-    flex: 4,
-    alignItems: 'flex-end',
-    marginRight: 30,
-  },
-  totalView: {
-    borderTopWidth: 2,
-
-    borderColor: 'black',
-  },
-});
+const styles = StyleSheet.create({});
